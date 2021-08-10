23448e514bc71a0c04af7e05455c0f8b183e4ad8
JorshuaG <joshbgearheart@gmail.com>
2021-08-10 13:00:51 -1000

    WIP, completed up to favoritemovies

---
+++ src/reducers/favoriteReducer.js
@@ -0,0 +1,13 @@
+const initialState = {
+  favorites: [],
+  displayFavorites: true,
+};
+
+const favReducer = (state = initialState, action) => {
+  switch (action.type) {
+    default:
+      return state;
+  }
+};
+
+export default favReducer;
